67dac6620f2a98196101dbfbc6ac8a618c791f42
Dmitry Parkhomenko <oku_74@mail.ru>
2023-11-14 23:26:51 +0500

    Developed the ViewController screen Add icon

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var labelView: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var rectangleView: UIView!
+    @IBOutlet weak var buttonView: UIButton!
+    
+    // MARK: - Properties
+    private let colors: [UIColor] = [.blue, .green, .red]
+    private var index = 0
+    
+    // MARK: - Lyfe cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupElements()
     }
-
-
+    
+    // MARK: - Private methods
+    private func setupElements() {
+        buttonView.layer.cornerRadius = 20
+        rectangleView.layer.cornerRadius = 50
+        imageView.isHidden = true
+        rectangleView.backgroundColor = .clear
+    }
+    
+    private func colorsChange() {
+        if index < colors.count {
+            rectangleView.backgroundColor = colors[index]
+            index += 1
+        } else {
+            index = 0
+        }
+    }
+    
+    // MARK: - IBActions
+    @IBAction func buttonTap() {
+        labelView.text = "Happy New Year!"
+        labelView.textColor = .green
+        imageView.isHidden.toggle()
+        colorsChange()
+    }
 }
 
